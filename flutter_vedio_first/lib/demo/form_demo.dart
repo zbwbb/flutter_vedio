55a2c196683c00ae48204d9df8abc2593632db34
zbwbb <zhoubinxinna@sina.com>
2019-01-07 22:53:23 +0800

    all

--- flutter_vedio_first/lib/demo/form_demo.dart
+++ flutter_vedio_first/lib/demo/form_demo.dart
@@ -4,7 +4,102 @@ class FormDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      // 可以在应用中的某一部分 定制一个特定的主题 这种写法会覆盖之前的主题
+      // body: Theme(
+      //   data: ThemeData(
+      //     primaryColor: Colors.red
+      //   ),
+      //   child: ThemeDemo(),
+      // ),
+
+      // 下面的写法：不会会覆盖之前的主题
+      body: Theme(
+        data: Theme.of(context).copyWith(
+          primaryColor: Colors.teal
+        ),
+        child: Container(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFieldDemo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ThemeDemo extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+      // 
+      return Container(
+        color: Theme.of(context).primaryColor,
+      );
+    }
+}
+
+/**
+ * 创建一个有状态的控件
+ */
+class TextFieldDemo extends StatefulWidget {
+  _TextFieldDemoState createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+
+  // 给文本添加一个控制器：1、初始化文本内容 2、监听文本内容的变化
+  // 使用 TextEditingController 监听文本字段变化
+  final  texteditcontroller = TextEditingController();
+
+  // 添加初始化数据
+  void initState() { 
+    super.initState();
+    // 文本的初始值
+    texteditcontroller.text = 'nihao';
+    // 添加一个监听器
+    texteditcontroller.addListener(
+      (){
+        debugPrint('input : ${texteditcontroller.text}');
+      }
+    );
+  }
+
+  // 控制器用完要消除
+  @override
+    void dispose() {
+      // 
+      texteditcontroller.dispose();
+      super.dispose();
+    }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: TextField(
+         // 监听回调
+        //  onChanged: (contet){
+        //    debugPrint(contet);
+        //  },
+
+        // 或者 这样监听回调
+        controller: texteditcontroller,
+
+         // 按下确定按钮
+         onSubmitted: (content){
+           debugPrint('提交的内容：'+ content);
+         },
+         decoration: InputDecoration(
+           icon: Icon(Icons.account_balance),
+           labelText: '姓名',
+           hintText: '请输入姓名',
+           // 边框 去除
+          //  border: InputBorder.none
+         ),
+       ),
     );
   }
 }
